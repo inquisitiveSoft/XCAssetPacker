849ab4cf651a30c7eb1c76044a1188c006c0c86a
Harry Jordan <harry@inquisitivesoftware.com>
2017-09-16 15:33:29 +0100

    Added support for 1024pt marketing app icons
    Updated to v1.2

--- XCAssetPacker/main.swift
+++ XCAssetPacker/main.swift
@@ -24,7 +24,7 @@ import Foundation
 import Cocoa
 
 
-let versionNumber = "1.11"
+let versionNumber = "1.2"
 
 // Setup command line options
 let inputPathOption = StringOption(shortFlag: "i", longFlag: "input", helpMessage: "Path to the input folder.")
